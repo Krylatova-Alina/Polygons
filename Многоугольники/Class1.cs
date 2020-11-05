6d51482806f2ba07da94d7c69e5c565dade4a645
Алина Крылатова <krylalina@gmail.com>
2020-11-05 12:00:02 +0300

    Добавьте файлы проекта.

---
+++ Многоугольники/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Многоугольники
+{  
+    public abstract class Vertex
+    {
+        protected int x0;
+        protected int y0;
+        public static int R
+        {
+            get; set;
+        }
+        public static Color col
+        {
+            get; set;
+        }
+        public int SetX { set { x0 = value; } get { return x0; } }
+        public int SetY { set { y0 = value; } get { return y0; } }
+        public Vertex()
+        {
+        }
+        public Vertex(int x, int y)
+        {            
+            this.x0 = x;
+            this.y0 = y;
+        }
+        static Vertex()
+        {
+            R = 40;
+            col = Color.Black;
+        }
+        abstract public void DrawFigure(Graphics graph);
+        abstract public bool Check(int x, int y);
+    }
+
+    class Circle : Vertex
+    {
+        public Circle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graph)
+        {
+            SolidBrush br = new SolidBrush(col);
+            graph.FillEllipse(br, x0 - R / 2, y0 - R / 2, R, R);
+        }
+        public override bool Check(int x, int y) 
+        {
+            if ((x - x0) * (x-x0) + (y-y0) * (y-y0) <= R * R /4)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Rectangle : Vertex
+    {
+        public Rectangle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graph)
+        {
+            SolidBrush br = new SolidBrush(col);
+            graph.FillRectangle(br, x0 - R/2, y0 - R/2, R, R);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (Math.Abs(x-x0) <= R/2 && Math.Abs(y - y0) <= R/2)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Triangle : Vertex
+    {
+        public Triangle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graph)
+        {
+            SolidBrush br = new SolidBrush(col);
+            Point point1 = new Point(x0 - R / 2, y0 + R / 2);
+            Point point2 = new Point(x0, y0 - R / 2);
+            Point point3 = new Point(x0 + R / 2, y0 + R / 2);
+            Point[] curvePoints = { point1, point2, point3 };
+            graph.FillPolygon(br, curvePoints);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (Math.Abs(x - x0) <= R / 2 && Math.Abs(y - y0) <= R / 2)
+
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Многоугольники/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Многоугольники
+{
+    public partial class Form1 : Form
+    {
+        Vertex shape;
+        int flagVertex = 0;
+        bool Draw = false;
+        int delX, delY;
+        bool DrawDrag;
+        public Form1()
+        {
+            InitializeComponent();
+            delX = delY = 0;
+            DrawDrag = false;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Draw)
+                shape.DrawFigure(e.Graphics);
+        }
+
+        private void кругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flagVertex = 0;
+        }
+        private void квадратToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flagVertex = 1;
+        }
+        private void треугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flagVertex = 2;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (DrawDrag)
+            {
+                shape.SetX = e.X - delX;
+                shape.SetY = e.Y - delY;
+            }
+            this.Refresh();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            DrawDrag = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (Draw && shape.Check(e.X, e.Y))
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    delX = e.X - shape.SetX;
+                    delY = e.Y - shape.SetY;
+                    DrawDrag = true;
+                }
+                if (e.Button == MouseButtons.Right)
+                {
+                    Draw = false;
+                    this.Refresh();
+                }
+            }
+            else
+            {
+                Draw = true;
+                switch (flagVertex)
+                {
+                    case 0: shape = new Circle(e.X, e.Y); break;
+                    case 1: shape = new Rectangle(e.X, e.Y); break;
+                    case 2: shape = new Triangle(e.X, e.Y); break;
+                }                
+            }
+            this.Refresh();
+        }       
+    }
+}
