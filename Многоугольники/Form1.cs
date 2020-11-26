5cf3ad7ffc48993a78076dbd6b401b50c444a6b4
Алина Крылатова <krylalina@gmail.com>
2020-11-26 05:02:26 +0300

    Уничтожение комментариев (Минизадача 3)

--- Многоугольники/Form1.cs
+++ Многоугольники/Form1.cs
@@ -12,19 +12,16 @@ namespace Многоугольники
 {
 	public partial class Form1 : Form
 	{
-		/*Vertex shape;*/
 		List<Vertex> shapes = new List<Vertex>();
 		int flagVertex = 0;
 		bool Draw = false;
 		List<int> delsX = new List<int>();
-		List<int> delsY = new List<int>();
-		/*int delX, delY;*/
+		List<int> delsY = new List<int>();		
 		bool DrawDrag;
 		List<bool> DrawDrags = new List<bool>();
 		public Form1()
 		{
-			InitializeComponent();
-			/*delX = delY = 0;*/
+			InitializeComponent();			
 			DrawDrag = false;
 		}
 
@@ -95,7 +92,7 @@ namespace Многоугольники
 
 		private void Form1_MouseMove(object sender, MouseEventArgs e)
 		{
-			if (DrawDrag/*DrawDrags.Contains(true)*/)
+			if (DrawDrag)
 			{
 				for (int i = 0; i < shapes.Count; i++)                   
 					if (DrawDrags[i])
